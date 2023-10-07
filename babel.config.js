cfa23fe5a51d07d321a719de8ee40a135508329d
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-10-07 19:10:39 -0300

    :sparkles: Added dependecies

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,30 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@dtos': './src/dtos',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@storage': './src/storage',
+          '@utils': './src/utils',
+          '@services': './src/services',
+          '@contexts': './src/contexts',
+          '@routes': './src/routes',
+          '@theme': './src/theme',
+          '@domain': './src/domain',
+          '@brand': './src/brand',
+          '@api': './src/api',
+          '@types': './src/types',
+          '@infra': './src/infra',
+        },
+      },],
   };
 };
