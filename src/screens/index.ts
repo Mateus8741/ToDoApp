54dfb628e80aa2c2bbc79687a58d60f8e229c9db
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-10-08 10:44:10 -0300

    :sparkles: SuccessScreen Added

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,4 +1,5 @@
-export * from './auth/LoginScreen/LoginScreen';
-export * from './auth/SignUpScreen/SignUpScreen';
+export * from './auth/LoginScreen/LoginScreen'
+export * from './auth/SignUpScreen/SignUpScreen'
+export * from './auth/SuccessScreen/SuccessScreen'
 
-export * from './app/HomeScreen/HomeScreen';
+export * from './app/HomeScreen/HomeScreen'
